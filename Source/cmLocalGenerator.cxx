b06615050293a04b7e7ef8ccf97ec706351d761e
Stephen Kelly <steveire@gmail.com>
2016-10-08 12:21:38 +0200

    cmLocalGenerator: Inline last use of local variable

--- Source/cmLocalGenerator.cxx
+++ Source/cmLocalGenerator.cxx
@@ -1403,8 +1403,6 @@ void cmLocalGenerator::OutputLinkLibraries(
 {
   OutputFormat shellFormat =
     (forResponseFile) ? RESPONSE : ((useWatcomQuote) ? WATCOMQUOTE : SHELL);
-  bool escapeAllowMakeVars = !forResponseFile;
-
   cmComputeLinkInformation& cli = *pcli;
 
   std::string linkLanguage = cli.GetLinkLanguage();
@@ -1456,7 +1454,7 @@ void cmLocalGenerator::OutputLinkLibraries(
   std::string rpath_link = cli.GetRPathLinkString();
   if (!cli.GetRPathLinkFlag().empty() && !rpath_link.empty()) {
     fout << cli.GetRPathLinkFlag();
-    fout << this->EscapeForShell(rpath_link, escapeAllowMakeVars);
+    fout << this->EscapeForShell(rpath_link, !forResponseFile);
     fout << " ";
   }
 
